5858f18cf3829ae32cdb01eeb4eb073850ac94c0
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-30 22:13:01 -0300

    Trigger Pipeline

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -29,3 +29,44 @@ resource "aws_vpc_peering_connection" "vpc_peering" {
   peer_vpc_id = aws_vpc.vpc10.id
   vpc_id      = aws_vpc.vpc20.id
 }
+
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.vpc10.id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  ingress {
+    description = "TCP/80 from All"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TCP/22 from All"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "sg-priv" {
+  vpc_id = aws_vpc.vpc10.id
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+}
+
--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -45,3 +45,58 @@ resource "azurerm_virtual_network_peering" "vnet-peering20" {
   virtual_network_name      = azurerm_virtual_network.vnet20.name
   remote_virtual_network_id = azurerm_virtual_network.vnet10.id
 }
+
+resource "azurerm_network_security_group" "nsgvm" {
+  name                = "nsgvm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "FTP"
+    priority                   = 1011
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsgsubnet1a" {
+  subnet_id                 = azurerm_subnet.subnet-pub.id
+  network_security_group_id = azurerm_network_security_group.nsgvm.id
+}
+
+resource "azurerm_network_security_group" "nsgvm-priv" {
+  name                = "nsgvm-priv"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  security_rule {
+    name                       = "Inbound-Internet-All"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsgsubnet1a" {
+  subnet_id                 = azurerm_subnet.subnet-priv.id
+  network_security_group_id = azurerm_network_security_group.nsgvm.id
+}
